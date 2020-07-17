e9eff4aa489afbc5a6f04f33385533b0c58dbc5c
jonahlipsky <jonahlipsky@gmail.com>
2020-07-17 18:11:34 -0400

    Initial commit

---
+++ src/root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+// import App from './App';
+
+export default () => {
+  return(
+    <>
+      <p className="initial">
+        hello world
+      </p>
+    </>
+  )
+};
